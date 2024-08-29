04de1991547751f87a346f36e13ed43e9e1a680f
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-29 16:50:11 +0800

    feat: implement login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return inertia('Auth/Login');
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $key = $request->input('email') ?: $request->ip();
+        $isAuthenticated = Auth::attempt($credentials, $request->remember);
+
+        if($isAuthenticated){
+            RateLimiter::clear($key);
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard')->with('greet', 'Welcome to BudgetBuddy');
+        }
+
+        RateLimiter::hit($key);
+        $tooManyAttempts = RateLimiter::tooManyAttempts($key, 3);
+
+        if($tooManyAttempts){
+            $retryAfter = RateLimiter::availableIn($key);
+
+            return back()->withErrors([
+                'email' => 'Too Many Attempts. Please try again in ' . $retryAfter . ' seconds.'
+            ])->onlyInput('email');
+        }
+
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records'
+        ])->onlyInput('email');
     }
 }
