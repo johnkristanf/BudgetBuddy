e80c6edde05f4769fb491e21f399537d1e9ce9e2
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-29 16:55:36 +0800

    feat: config ziggy for route link

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Head, Link} from '@inertiajs/vue3';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
 import Layout from './Layout/Layout.vue';
 
 createInertiaApp({
@@ -10,13 +12,20 @@ createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
     let page =  pages[`./Pages/${name}.vue`]
-    page.default.layout = page.default.layout || Layout
+    
+    if (name === 'Auth/Login') {
+      page.default.layout = null;
+    } else {
+      page.default.layout = page.default.layout || Layout;
+    }
+    
     return page;
   },
 
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .component('Head', Head)
       .component('Link', Link)
       .mount(el)
