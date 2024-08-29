eed6116b3a7bec0ce30153099ef1923cc47af4c2
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-29 16:55:53 +0800

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('login', function (Request $request) {
+            $key = $request->input('email') ?: $request->ip();
+            $tooManyAttempts = RateLimiter::tooManyAttempts($key, 3);
+
+            if($tooManyAttempts){
+                $retryAfter = RateLimiter::availableIn($key);
+
+                return back()->withErrors([
+                    'email' => 'Too Many Attempts. Please try again in ' . $retryAfter . ' seconds.'
+                ])->onlyInput('email');
+            }
+
+            return Limit::perMinutes(20, 3)->by($key);
+        });
     }
 }
