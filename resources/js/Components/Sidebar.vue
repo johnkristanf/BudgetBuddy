ed93ebc342b88646d0ec6ecb6f64333b0ee60059
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-31 03:54:14 +0800

    style: sidebar and the navigation links

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,20 +1,37 @@
 <script setup>
+    import logo from '../../../public/img/budgetbuddy_logo.jpg';
 
     const links = [
-        {route: 'dashboard', component: 'Dashboard'},
-        {route: 'transactions', component: 'Transactions'},
-    ]
+        { route: 'dashboard', component: 'Dashboard', icon: ['fas', 'gauge'] },
+        { route: 'transactions', component: 'Transactions', icon: ['fas', 'money-bill'] },
+        { route: 'notifications', component: 'Notifications', icon: ['fas', 'gear'] },
+        { route: 'settings', component: 'Settings', icon: ['fas', 'gear'] },
+    ];
+
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <Link 
-            v-for="link in links"
-            :key="link.route"
-            :href="route(link.route)"
-            :class="{'bg-red-800': $page.component == link.component}"
+    <div 
+        class="w-[25%] h-screen fixed top-0 left-0 bg-blue-600 flex flex-col gap-5 items-center font-semibold text-white p-8"
+    >
+        <a href="#" class="flex items-center mb-6 mt-10 text-3xl font-semibold text-white dark:text-white">
+            <img class="w-12 h-12 mr-2 rounded-full" :src="logo" alt="logo">
+            Budget Buddy    
+        </a>
+
+        <div class="flex flex-col gap-12 w-full uppercase mt-3">
+            <Link 
+                v-for="link in links"
+                :key="link.route"
+                :href="$page.component == link.component ? null : route(link.route)"
+                :class="[
+                    'p-2 rounded-md w-full text-center',
+                    $page.component == link.component ? 'bg-white text-black' : 'hover:bg-black hover:text-white'
+                ]"
             >
-               {{ link.component }}
-        </Link>
+                <font-awesome-icon :icon="link.icon" class="text-xl"/>
+                {{ link.component }}
+            </Link>
+        </div>
     </div>
 </template>
--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -1,6 +1,8 @@
 
 <script setup>
     import Sidebar from '../Components/Sidebar.vue';
+    import logo from '../../../public/img/budgetbuddy_logo.jpg';
+
 </script>
 
 
@@ -12,6 +14,19 @@
 
     <div>
         <Sidebar />
+
+        <div class="w-full flex justify-end h-28">
+            <div class="flex items-center w-[73%] h-full justify-between p-5">
+                <h1 class="font-semibold text-2xl uppercase">Budget Buddy {{ $page.component }}</h1>
+
+                <div class="flex items-center gap-2 hover:cursor-pointer">
+                    <img :src="logo" alt="User Profile Picture" class="w-12 rounded-full">
+                    <h1 class="font-semibold text-lg">{{ $page.props.auth.user.name }}</h1>
+
+                </div>
+            </div>
+        </div>
+
         <main class="p-4">
             <slot />
         </main>
