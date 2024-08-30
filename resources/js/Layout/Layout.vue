5862399d4930630f219901fb56ce1d9f84a7b47b
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-31 05:52:07 +0800

    style: logout button

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -2,7 +2,14 @@
 <script setup>
     import Sidebar from '../Components/Sidebar.vue';
     import logo from '../../../public/img/budgetbuddy_logo.jpg';
+    import { ref } from 'vue';
 
+    const props = defineProps({
+        flash: Object
+    })
+
+    const flashMessage = ref(props.flash.message);
+    setTimeout(() => flashMessage.value = null, 5000);
 </script>
 
 
@@ -12,6 +19,16 @@
         <link rel="icon" href="/img/budgetbuddy_logo.jpg" type="image/x-icon">
     </Head>
 
+    <div class="w-full fixed top-2 z-[9999] flex justify-center">
+        <p 
+            v-if="flashMessage"
+            class="w-[50%] p-5 bg-blue-600 rounded-md text-white font-semibold text-center ml-5"
+            >
+
+            {{ $page.props.flash.message }} {{ $page.props.auth.user.name }} <font-awesome-icon :icon="['fas', 'face-smile']" />
+        </p>
+    </div>
+
     <div>
         <Sidebar />
 
@@ -19,11 +36,24 @@
             <div class="flex items-center w-[73%] h-full justify-between p-5">
                 <h1 class="font-semibold text-2xl uppercase">Budget Buddy {{ $page.component }}</h1>
 
-                <div class="flex items-center gap-2 hover:cursor-pointer">
+                <div class="flex items-center gap-3 hover:cursor-pointer">
                     <img :src="logo" alt="User Profile Picture" class="w-12 rounded-full">
                     <h1 class="font-semibold text-lg">{{ $page.props.auth.user.name }}</h1>
 
+                    <Link 
+                        type="button"
+                        method="post"
+                        as="button"
+                        :href="route('logout')"
+                    >
+                        <font-awesome-icon 
+                            :icon="['fas', 'right-from-bracket']"
+                            class="text-xl" 
+                        />
+                    </Link>
+                    
                 </div>
+
             </div>
         </div>
 
